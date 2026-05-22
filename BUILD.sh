e31385b4f498b9974410df1326fea5e9c1de9b92
Chris-AC9KH <chriskristin.olson@icloud.com>
2026-05-21 19:17:32 -0500

    Update build script to copy only boost headers and don't build the binaries

--- BUILD.sh
+++ BUILD.sh
@@ -56,7 +56,7 @@ fi
     make clean
     clear
     echo "--------------------------------------------------------------------"
-    echo "         Hamlib-v4.7.0 build successful........."
+    echo "         Hamlib-v4.7.1 build successful........."
     echo "--------------------------------------------------------------------"
     sleep 5
 
@@ -79,16 +79,13 @@ fi
 ####### Build boost #######
 cd ../boost
 ./bootstrap.sh --prefix=${PREFIX}
-if [ "$choice" = "y" ]; then
-    ./b2 cxxflags="-mmacosx-version-min=12.0" -a address-model=64 architecture=arm+x86 install
-else
-    ./b2 cxxflags="-mmacosx-version-min=12.0" -a install
-fi
-    clear
-    echo "--------------------------------------------------------------------"
-    echo "         boost-v1.88.0 build successful........."
-    echo "--------------------------------------------------------------------"
-    sleep 5
+./b2 install --with-headers
+
+clear
+echo "--------------------------------------------------------------------"
+echo "         boost-v1.88.0 header copy successful........."
+echo "--------------------------------------------------------------------"
+sleep 5
 
 read -p "Build Qt6 from git sources? Select No if using external Qt build: Yes(y) / No(n):- " qt
 
